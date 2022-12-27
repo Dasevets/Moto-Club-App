67ba7537a245499cdbb633680c5cfddfc8b833da
Dashevets <walter0459@gmail.com>
2022-12-27 14:21:02 +0300

    Contacts in progress MVVM architecture in progress Horizontal orientation in progress

--- app/src/main/java/com/example/clubapp/view/rv_adapter/ServicesAdapter.kt
+++ app/src/main/java/com/example/clubapp/view/rv_adapter/ServicesAdapter.kt
@@ -3,8 +3,10 @@ package com.example.clubapp.view.rv_adapter
 import android.view.LayoutInflater
 import android.view.View.*
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.clubapp.R
 import com.example.clubapp.data.Service
 import com.example.clubapp.databinding.ServicesBinding
 
@@ -16,6 +18,7 @@ class ServicesAdapter(private val servicesDB: List<Service>, private val onClick
         RecyclerView.ViewHolder(bindingServ.root) {
         val titleServ: TextView = bindingServ.title
         val serv: TextView = bindingServ.service
+        var collapseIM : ImageView = bindingServ.collapseImage
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ServicesViewHolder {
@@ -26,14 +29,22 @@ class ServicesAdapter(private val servicesDB: List<Service>, private val onClick
     override fun onBindViewHolder(holder: ServicesViewHolder, position: Int) {
         holder.titleServ.text = servicesDB[position].title
         holder.serv.text = servicesDB[position].serv
-
+        holder.collapseIM.setImageResource(R.drawable.collapse_icon_close)
 
 
         holder.itemView.setOnClickListener() {
             onClickListener.clickServ(servicesDB[position])
 
-            if(holder.serv.visibility == GONE) { holder.serv.visibility = VISIBLE}
-            else holder.serv.visibility = GONE
+            if(holder.serv.visibility == GONE) {
+                holder.serv.visibility = VISIBLE
+                holder.collapseIM.setImageResource(R.drawable.collapse_icon_open)
+
+            }
+            else {
+                holder.serv.visibility = GONE
+                holder.collapseIM.setImageResource(R.drawable.collapse_icon_close)
+
+            }
 
         }
     }
